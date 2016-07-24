7b9c5ca87cd6f3efecfed8984564315218a54d61
Sappawish Siripon <sisappawish@col.co.th>
2016-07-24 22:32:57 +0700

    Custom SELECT (UI)
    - add custom SELECT JS
    - split JS custom SELECT from mobile & desktop

--- js/main.js
+++ js/main.js
@@ -15,4 +15,23 @@ jQuery(document).ready(function($) {
           }
         });
     }
+
+    /*----------  Detect Touch Device and use custom dropdown  ----------*/
+    if (Modernizr.touchevents && Modernizr.mq("screen and (min-width: 1280px)")) { 
+      
+      console.log('Surface Pro');
+      $('.select-custom select').selectize();
+
+    } else if(Modernizr.touchevents ) { 
+      
+      console.log('Mobile or Tablet Touch Screen');
+      $('.select-custom select').customSelect();
+      $('.select-custom-type select').selectize()
+
+    } else { 
+      
+      console.log('Desktop Browser');
+      $('.select-custom select').selectize();
+
+    }
 });
